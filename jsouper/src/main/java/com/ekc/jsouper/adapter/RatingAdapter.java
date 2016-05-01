9f3d066a5594ba04d0302e5c424f4fb04a5d6f03
Erick Chang <ekc.dev@gmail.com>
2016-05-01 17:45:34 -0400

    Rating with stars as a double

--- jsouper/src/main/java/com/ekc/jsouper/adapter/RatingAdapter.java
+++ jsouper/src/main/java/com/ekc/jsouper/adapter/RatingAdapter.java
@@ -3,9 +3,13 @@ package com.ekc.jsouper.adapter;
 import com.ekc.jsouper.ElementAdapter;
 import com.ekc.jsouper.models.Rating;
 import java.io.IOException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import org.jsoup.nodes.Element;
 
 public class RatingAdapter extends ElementAdapter<Rating> {
+  static final Pattern RATING_PATTERN = Pattern.compile("(\\d*)\\.(\\d*)");
+
   @Override
   public String query() {
     return "div.reason-set";
@@ -16,7 +20,16 @@ public class RatingAdapter extends ElementAdapter<Rating> {
     String starsDescription = element.select("div.tiny-star.star-rating-non-editable-container")
         .first()
         .attr("aria-label");
+
+    double ratingPercent = 0d;
+    final Matcher matcher =
+        RATING_PATTERN.matcher(element.select("div.current-rating").attr("style"));
+    if (matcher.find()) {
+      ratingPercent = Double.valueOf(matcher.group());
+    }
+
     String price = element.select("span.display-price").text();
-    return new Rating(starsDescription, price);
+
+    return new Rating(starsDescription, ratingPercent, price);
   }
 }
--- jsouper/src/main/java/com/ekc/jsouper/models/Rating.java
+++ jsouper/src/main/java/com/ekc/jsouper/models/Rating.java
@@ -2,15 +2,17 @@ package com.ekc.jsouper.models;
 
 public class Rating {
   public final String starsDescription;
+  private final double ratingPercent;
   public final String price;
 
-  public Rating(String starsDescription, String price) {
+  public Rating(String starsDescription, double ratingPercent, String price) {
     this.starsDescription = starsDescription;
+    this.ratingPercent = ratingPercent;
     this.price = price;
   }
 
   @Override
   public String toString() {
-    return String.format("%s\n%s", starsDescription, price);
+    return String.format("%s\n%f\n%s\n", starsDescription, ratingPercent, price);
   }
 }
