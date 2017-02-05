cd1a46c297bf0eddbf421edeed3057e5c97a5134
futabooo <mail@futabooo.com>
2017-02-05 22:48:01 +0900

    fix sample-android project, NullPointerException

--- sample-android/src/main/java/com/ekchang/jsouper/sample/api/PlayStoreApi.java
+++ sample-android/src/main/java/com/ekchang/jsouper/sample/api/PlayStoreApi.java
@@ -8,6 +8,6 @@ import retrofit2.http.GET;
 public interface PlayStoreApi {
   String BASE_URL = "https://play.google.com/store/";
 
-  @GET("movies/collection/promotion_400079b_most_popular_movies")
+  @GET("movies/collection/promotion_4000a71_newrelease_mo_eg_ac")
   Call<List<Movie>> getMovies();
 }
